a6f6aa12f4aa199f4291271a78b9492993bb75eb
Onyekachi Fakoya <88961671+kachifako@users.noreply.github.com>
2022-10-15 10:32:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
 
-function App() {
+
+
+
+
+export default function App() {
+
+  let operator = ['/', '+', '-', '*', '=', '.']
+
+  const [output, setOutput] = useState("");
+  const [result, setResult] = useState("");
+
+
+
+  const updateOutput = value => {
+    
+   if (operator.includes(value) && output === '' || operator.includes(value) && operator.includes(output.slice(-1))) {
+      return '';
+    }
+
+    
+
+    setOutput((prev) => prev += value)
+
+
+    setResult(eval(output + value))
+
+  }
+
+
+  const deleteOutput = () => {
+    
+    setOutput((value) => value = output.slice(0, -1));
+
+   
+  }
+
+  const clearOutput = () => {
+    setResult(value => value = '')
+    setOutput(value => value = '')
+  }
+
+  const evaluateOutput = () => {
+
+    setOutput(value => value = eval(output))
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <div className='container'>
+        <h5>@kachifako</h5>
+        <div className='display'>
+          <span>{output}</span>
+          <br></br>
+          <span className='results'>{result}</span>
+        </div>
 
-export default App;
+
+        <div className='btn-div'>
+          <button onClick={() => clearOutput()} className='clear btns'> C</button>
+          <button onClick={() => deleteOutput()} className='btns'> DEL</button>
+          <button onClick={() => updateOutput('/')} className='btns'> /</button>
+          <button onClick={() => updateOutput('1')} className='btns'> 1</button>
+          <button onClick={() => updateOutput('2')} className='btns'> 2</button>
+          <button onClick={() => updateOutput('3')} className='btns'>3 </button>
+          <button onClick={() => updateOutput('*')} className='btns'> x</button>
+          <button onClick={() => updateOutput('4')} className='btns'>4 </button>
+          <button onClick={() => updateOutput('5')} className='btns'> 5</button>
+          <button onClick={() => updateOutput('6')} className='btns'> 6</button>
+          <button onClick={() => updateOutput('-')} className='btns'> -</button>
+          <button onClick={() => updateOutput('7')} className='btns'> 7</button>
+          <button onClick={() => updateOutput('8')} className='btns'> 8</button>
+          <button onClick={() => updateOutput('9')} className='btns'> 9</button>
+          <button onClick={() => updateOutput('+')} className='btns'> +</button>
+          <button onClick={() => updateOutput('.')} className='btns'> .</button>
+          <button onClick={() => updateOutput('0')} className='btns'> 0</button>
+          <button onClick={() => evaluateOutput()} value='=' className='equals btns'> = </button>
+        </div>
+
+
+      </div>
+    </main>
+  )
+}
